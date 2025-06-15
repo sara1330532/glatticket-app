6d326c849034950f939ebb1e8c0995a773a34a50
sara <sw8551330@gmail.com>
2025-06-15 20:17:36 +0300

    fix: match file name casing for Netlify

--- src/pages/AddTicket.jsx
+++ src/pages/AddTicket.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { apiAddTickets } from "../api/ticketservice";
+import { apiAddTickets } from "../api/ticketService";
 import { TextField, Button, Box, Typography, Container } from "@mui/material";
 
 // ======================================================
